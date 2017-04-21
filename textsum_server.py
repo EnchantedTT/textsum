415f258b5a121bd8e02f939874aa6af04109435d
EnchantedTT <lcj199134@gmail.com>
2017-04-21 11:17:38 +0800

    add templates for one request

--- textsum_server.py
+++ textsum_server.py
@@ -5,6 +5,7 @@ import logging
 #import settings
 import utils
 import textsum
+from flask import render_template
 
 app = Flask(__name__)
 FlaskJSON(app)
@@ -24,5 +25,37 @@ def getSummarization():
 		raise e
 	return jsonify(result)
 
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/summary/one",methods = ['GET', 'POST'])
+def login():
+	if request.method == "POST":
+		try:
+			username = request.form.get('username')
+			topics = [username]
+			output = textsum.getSummarizations(topics)
+			result = output[0]
+			return "<p>Result: " + result['result'] + "</p><p>Score: " + str(result['score']) + "</p>"
+		except:
+			return "<h1>Invalid Input.</h1>"    
+	else:
+		return "<h1>Invalid Input.</h1>"
+
+@app.route("/summary/one", methods=['GET'])
+def getSummarizationForOne():
+	try:
+		raw = request.args
+		result = dict()
+		topics = [raw[0][1]]
+		output = textsum.getSummarizations(topics)
+		result['output'] = output
+	except Exception as e:
+		#LOGGER.info("Summarization Server: " + str(type(e)))
+		raise JsonError(description='Invalid Input.')
+	return render_template('index.html', name=name)
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=50110, threaded=True)
+	#app.run(host='0.0.0.0', port=50110, threaded=True)
+	app.run(host='0.0.0.0', port=50110, debug=True)
